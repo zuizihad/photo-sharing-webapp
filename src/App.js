3dbe6422c0a3d02d62d0023ba9900fd5255814bc
zuizihad <ahmedxihad@gmail.com>
2021-04-29 04:42:34 +0600

    user created

--- src/App.js
+++ src/App.js
@@ -9,43 +9,46 @@ import NotFound from './components/NotFound/NotFound';
 import Navbar from './components/Shared/Navbar/Navbar';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 import Profile from './components/Profile/Profile';
 import UploadPhoto from './components/UploadPhoto/UploadPhoto';
 import CreateAlbum from './components/CreateAlbum/CreateAlbum';
 
 export const userContext = createContext();
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState({});
   return (
-    <Router>
-      <Navbar></Navbar>
-      <Switch>
-        <Route exact path="/">
-          <Home></Home>
-        </Route>
-        <Route path="/home">
-          <Home></Home>
-        </Route>
-        <Route path="/login">
-          <Login></Login>
-        </Route>
-        <Route path="/register">
-          <Register></Register>
-        </Route>
-        <Route path="/profile">
-          <Profile></Profile>
-        </Route>
-        <Route path="/createAlbum">
-          <CreateAlbum></CreateAlbum>
-        </Route>
-        <Route path="/uploadPhoto">
-          <UploadPhoto></UploadPhoto>
-        </Route>
-        <Route path="*">
-          <NotFound></NotFound>
-        </Route>
-      </Switch>
-    </Router>
+    <userContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <Router>
+        <Navbar></Navbar>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/login">
+            <Login></Login>
+          </Route>
+          <Route path="/register">
+            <Register></Register>
+          </Route>
+          <Route path="/profile">
+            <Profile></Profile>
+          </Route>
+          <Route path="/createAlbum">
+            <CreateAlbum></CreateAlbum>
+          </Route>
+          <Route path="/uploadPhoto">
+            <UploadPhoto></UploadPhoto>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
+    </userContext.Provider>
   );
 }
 
--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,25 +1,60 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
+import { userContext } from '../../App';
 const Register = () => {
+    const [user, setUser] = useState({
+        name: '',
+        email: '',
+        password: '',
+    })
+    const [loggedInUser, setLoggedInUser] = useContext(userContext)
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = (data, e) => {
+        e.preventDefault()
+        try {
+            const userData = {
+                name: data.name,
+                email: data.email,
+                password: data.password
+            }
+            setLoggedInUser(userData)
+            const url = `http://localhost:5000/user/register`
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(userData)
+            })
+                .then(response => {
+                    alert('user created success')
+                    console.log(response.data)
+                })
+        } catch (err) {
+            alert(err.response.data.msg)
+        }
+
+    }
     return (
         <div className="d-flex justify-content-center mt-5">
             <div className="card login-card ">
                 <div className="card-body ">
                     <h2 className="text-center text-secondary my-3">Register</h2>
-                    <form action="">
+                    <form onSubmit={handleSubmit(onSubmit)}>
                         <div class="input-group mb-3">
                             <span class="input-group-text" id="basic-addon1">N</span>
-                            <input type="text" class="form-control" placeholder="Name" aria-label="name" aria-describedby="basic-addon1" />
+                            <input type="text" {...register("name", { required: true })} class="form-control" placeholder="Name" aria-label="name" aria-describedby="basic-addon1" />
                         </div>
                         <div class="input-group mb-3">
                             <span class="input-group-text" id="basic-addon1">E</span>
-                            <input type="text" class="form-control" placeholder="Email" aria-label="password" aria-describedby="basic-addon1" />
+                            <input {...register("email", { required: true })} type="text" class="form-control" placeholder="Email" aria-label="password" aria-describedby="basic-addon1" />
                         </div>
                         <div class="input-group mb-3">
                             <span class="input-group-text" id="basic-addon1">P</span>
-                            <input type="text" class="form-control" placeholder="Password" aria-label="password" aria-describedby="basic-addon1" />
+                            <input {...register("password", { required: true })} type="password" class="form-control" placeholder="Password" aria-label="password" aria-describedby="basic-addon1" />
                         </div>
-                        <div className="text-center"><button type="button" class="btn btn-secondary ">sign up</button></div>
+                        <div className="text-center"><button type="submit" class="btn btn-secondary ">sign up</button></div>
                         <div><p>already have an account?<Link to="/login">login</Link></p></div>
                     </form>
                 </div>
